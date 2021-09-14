73f1e91fcae8de2f5d7bd062644b274d1482375e
18058521-KhanhVo <khanhvo395@gmail.com>
2021-09-14 12:16:48 +0700

    first commit JPQL-branch

--- src/main/java/com/example/StudentSpringDataJpa/service/StudentService.java
+++ src/main/java/com/example/StudentSpringDataJpa/service/StudentService.java
@@ -3,7 +3,7 @@ package com.example.StudentSpringDataJpa.service;
 import com.example.StudentSpringDataJpa.entity.Student;
 
 public interface StudentService {
-    public Student saveStudent(Student student);
+    public void saveStudent(Student student);
     public Student getStudent(int id);
     public void update(Student student);
     public void delete(Student student);
