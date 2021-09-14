b8bd7ecd743e13b7e0869e3577df99f2776e0014
18058521-KhanhVo <khanhvo395@gmail.com>
2021-09-14 08:59:32 +0700

    first commit master-branch

---
+++ src/test/java/com/example/StudentSpringDataJpa/StudentSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentSpringDataJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
